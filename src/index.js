185d274dbc2e8d3aeb0db0db9fd77c817c275d2f
yuriykolisnyk <ykolisnyk@gmail.com>
2021-09-23 16:31:18 +0300

    Add modal

--- src/index.js
+++ src/index.js
@@ -1,34 +1,67 @@
-import itemsDefault from "./app.js";
+import galleryItems from "./app.js";
 
-const refs = {
-  galleryList: document.querySelector(".js-gallery"), //общий родитель картинок
-  modal: document.querySelector(".js-lightbox"), //модальное окно
-  modalCloseBtn: document.querySelector('button[data-action="close-lightbox"]'), //кнопка закрытия модального окна
-  modalImage: document.querySelector(".lightbox__image"),
-  modalOverlay: document.querySelector(".lightbox__overlay"), //серый фон в модалке
-};
+const galleryContainer = document.querySelector(".js-gallery");
+const modal = document.querySelector(".js-lightbox");
+const modalImg = document.querySelector(".lightbox__image");
+// const modalContent = document.querySelector(".lightbox__image");
+const overlay = document.querySelector(".lightbox__overlay");
+const modalBtnClose = document.querySelector(".lightbox__button");
+// const modalBtnRight = document.querySelector(".scroll-right");
+// const modalBtnLeft = document.querySelector(".scroll-left");
 
-const galleryMarkup = createGalleryMarkup(itemsDefault);
-refs.galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", modalOpen);
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  galleryCardMarkup(galleryItems)
+);
 
-function createGalleryMarkup(elements) {
-  return elements
+function galleryCardMarkup(img) {
+  return img
     .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${original}"
-  >
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-    `;
+      return `<li class="gallery__item">
+                    <a class="gallery__link"
+                     href=${original}>
+                         <img class="gallery__image"
+                          src=${preview}
+                          data-source=${original}
+                          alt=${description} />
+                    </a>
+                    </li>`;
     })
     .join("");
+}
+
+function modalOpen(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  modal.classList.add("is-open");
+  modalImg.src = event.target.dataset.source;
+  modalImg.alt = event.target.alt;
+  overlay.addEventListener("click", modalCloseByOverlayClick);
+  document.addEventListener("keydown", modalCloseByEsc);
+  modalBtnClose.addEventListener("click", modalClose);
+  window.addEventListener("keydown", modalImgScrolling);
+}
+
+function modalClose(event) {
+  modal.classList.remove("is-open");
+  overlay.removeEventListener("click", modalCloseByOverlayClick);
+  document.removeEventListener("keydown", modalCloseByEsc);
+  modalBtnClose.removeEventListener("click", modalClose);
+  window.removeEventListener("keydown", modalImgScrolling);
+}
+
+function modalCloseByEsc(event) {
+  if (event.code === "Escape") {
+    modalClose(event);
+  }
+}
+
+function modalCloseByOverlayClick(event) {
+  if (event.currentTarget === event.target) {
+    modalClose(event);
+  }
 }
